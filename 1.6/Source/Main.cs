375e9815d2017eb0c570052eb95f5ec0110a1918
Epicmoofish <bender.erict@gmail.com>
2026-06-04 18:47:06 -0700

    starting

--- 1.6/Source/Main.cs
+++ 1.6/Source/Main.cs
@@ -7,10 +7,8 @@ namespace OOPhoenixLords
 {
     public class OOPhoenixLordsMod : Mod
 	{
-		Settings settings;
         public OOPhoenixLordsMod(ModContentPack content) : base(content)
         {
-            this.settings = GetSettings<Settings>();
             var harmony = new Harmony("oophoenixlords");
             harmony.PatchAll();
         }
